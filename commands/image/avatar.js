b1ca0c3735b986bf51ae578131b7ab261d349bfa
stovven <29066962+Stovven@users.noreply.github.com>
2021-05-08 23:06:08 -0400

    Create avatar.js

---
+++ commands/image/avatar.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const id = "218919888583000064"
+
+module.exports = {
+  commands: 'avatar' ,
+  description: 'changes the avatar, completely usable for everyone',
+  callback: (message, arguments, text, client) => {
+  if(message.author.id === id) {
+    message.delete({reason: "dont worry this is normal"})
+    if(arguments.length === 0) return message.channel.send('dumbass send some image link')
+    let avatar = arguments[0]
+    client.user.setAvatar(avatar).catch(error => {
+      message.channel.send(`we've done fucked up please use a vaild link`)
+    })
+  } else {
+    let embed = new Discord.MessageEmbed()
+    .setColor('#47FC74')
+    .setAuthor("no!")
+    .setDescription(`fuck off, like really`)
+    message.channel.send(embed)
+  }
+  },
+}
