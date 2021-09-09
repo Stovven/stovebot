26ce6fa8bf3c3baebf29f6a6a87e0542163c4b9e
stovven <29066962+Stovven@users.noreply.github.com>
2021-09-09 01:22:00 -0400

    andy push

--- commands/text/its-okay-i-guess.js
+++ commands/text/its-okay-i-guess.js
@@ -18,81 +18,88 @@ module.exports = {
       return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
     }
     const time = randomDate(new Date(2020, 18, 3), new Date());
-    time.setDate(time.getDate() - 2)
-    //console.log(time)
-    let snowflakeGen = Discord.SnowflakeUtil.generate(time)
-    //console.log(snowflakeGen)
+    const timeSecondMesssage = randomDate(new Date(2020, 18, 3), new Date());
+    const timeThirdMessage = randomDate(new Date(2020, 18, 3), new Date());
 
-    //pick our channel
-    //console.log(itsOkayChannel)
-    let randomChannel = itsOkayChannel[Math.floor(Math.random() * itsOkayChannel.length)];
-    //console.log(randomChannel)
-    let fetchedChannel = await client.channels.fetch(`${randomChannel}`)
-    //console.log(fetchedChannel)
-    if (!fetchedChannel) return console.log(`The channel ${randomChannel} is invaild, check config again!`)
-    let channelMessages = await fetchedChannel.messages.fetch({limit: 30, before: `${snowflakeGen}`})
-    let filteredMessages = channelMessages.filter(m => m.content !== '')
-    //console.log(filteredMessages)
-    function getRandomItem(set) {
-      let items = Array.from(set);
-      return items[Math.floor(Math.random() * items.length)];
-  }
-    let randomMessageMain = getRandomItem(filteredMessages)
-    let randomMessageSecond = getRandomItem(filteredMessages)
-    let randomMessageThird = getRandomItem(filteredMessages)
-    //console.log(`answer: `, randomMessageMain[1].author.username)
-    while (randomMessageSecond[1].author.username == randomMessageMain[1].author.username || randomMessageSecond[1].author.username == randomMessageThird[1].author.username) {
-      let count1 = 0 
-      count1 = count1 + 1
-     // console.log(count1)
-      if(count1 < 30) {
-      randomMessageSecond = getRandomItem(filteredMessages)
-     // console.log(randomMessageSecond[1].author.username)
-      }
-      if (count1 = 30) {
-        //console.log(`we fucked up, choosing whatever's left`)
-        break
-      }
+    timeSecondMesssage.setDate(timeSecondMesssage.getDate() - Math.floor(Math.random() * 11))
+    timeThirdMessage.setDate(timeThirdMessage.getDate() - Math.floor(Math.random() * 11 + 5))
+
+  let snowflakeGen1 = Discord.SnowflakeUtil.generate(time)
+  let snowflakeGen2 = Discord.SnowflakeUtil.generate(timeSecondMesssage)
+  let snowflakeGen3 = Discord.SnowflakeUtil.generate(timeThirdMessage)
+
+  //pick our channel
+  let randomChannel = itsOkayChannel[Math.floor(Math.random() * itsOkayChannel.length)];
+  let fetchedChannel = await client.channels.fetch(`${randomChannel}`)
+  if (!fetchedChannel) return console.log(`The channel ${randomChannel} is invaild, check config again!`)
+
+  let channelMessages = await fetchedChannel.messages.fetch({limit: 5, before: `${snowflakeGen1}`})
+  let filteredMessagesOnce = channelMessages.filter(m => m.author.id !== itsNotOkayUserID.find((id) => id === m.author.id))
+  let filteredMessages = filteredMessagesOnce.filter(m => m.content !== '')
+
+  let channelMessages1 = await fetchedChannel.messages.fetch({limit: 5, before: `${snowflakeGen2}`})
+  let filteredMessagesTwice = channelMessages1.filter(m => m.author.id !== itsNotOkayUserID.find((id) => id === m.author.id))
+  let filteredMessages2 = filteredMessagesTwice.filter(m => m.content !== '')
+
+  let channelMessages2 = await fetchedChannel.messages.fetch({limit: 5, before: `${snowflakeGen3}`})
+  let filteredMessagesThrice = channelMessages2.filter(m => m.author.id !== itsNotOkayUserID.find((id) => id === m.author.id))
+  let filteredMessages3 = filteredMessagesThrice.filter(m => m.content !== '')
+
+
+  function getRandomItem(set) {
+    let items = Array.from(set);
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+  let randomMessageMain = getRandomItem(filteredMessages)
+  let randomMessageSecond = getRandomItem(filteredMessages2)
+  let randomMessageThird = getRandomItem(filteredMessages3)
+
+  do {
+    randomMessageSecond = getRandomItem(filteredMessages2)
+    let count1 = 0 
+    count1 = count1 + 1
+    if (count1 = 5) break
+  } while (randomMessageSecond[1].author.username === randomMessageMain[1].author.username)
+
+  do {
+    randomMessageThird = getRandomItem(filteredMessages3)
+    let count1 = 0 
+    count1 = count1 + 1
+    if (count1 = 5) break
+  } while (randomMessageThird[1].author.username === randomMessageMain[1].author.username && randomMessageSecond[1].author.username === randomMessageThird[1].author.username)
+
+  let arr = [ ]
+  arr.push(randomMessageMain[1].author.username)
+  arr.push(randomMessageSecond[1].author.username)
+  arr.push(randomMessageThird[1].author.username)
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-     while (randomMessageThird[1].author.username === randomMessageMain[1].author.username || randomMessageSecond[1].author.username === randomMessageThird[1].author.username) {
-      let count2 = 0 
-      count2 = count2 + 1
-      //console.log(count2)
-      if(count2 < 30){ 
-      randomMessageThird = getRandomItem(filteredMessages)
-     // console.log(randomMessageThird[1].author.username)
-    }
-      if (count2 = 30) {
-       // console.log(`we fucked up, choosing whatever's left`)
-        break
-      }
-    }
-    //console.log(randomMessageSecond[1].author.username)
-    //console.log(randomMessageThird[1].author.username)
-    let arr = [ ]
-    arr.push(randomMessageMain[1].author.username)
-    arr.push(randomMessageSecond[1].author.username)
-    arr.push(randomMessageThird[1].author.username)
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-      }
-  }
-  shuffleArray(arr)
-  //console.log(arr)
-  let embed = new Discord.MessageEmbed()
-    .setColor('#47FC74')
-    .setDescription(`Here's this new random message: "${randomMessageMain[1].content}"\n Guess who it is! Could it be\n 1.**${arr[0]}**\n2.**${arr[1]}**\n3.**${arr[2]}**\n30 seconds starts now (so dont fuck up)!\n (use usernames not numbers idiot)`)
-    message.channel.send({embeds: [embed]})
-    let userFilter = m => m.author.id === message.author.id
-    let userResponse = await message.channel.awaitMessages({filter: userFilter, max: 1, time: 30000, errors: ['time']}).catch(error => {
-      message.channel.send(`Timeout due to inactivity, the correct answer was ${randomMessageMain[1].author.username}`)
-      return
-  })
-  if (typeof userResponse == 'undefined') {
+}
+
+shuffleArray(arr)
+
+
+let embed = new Discord.MessageEmbed()
+  .setColor('#2F3136')
+  .setTitle(`Here's a new random message!`)
+  .setDescription(`\n\`\`\`${randomMessageMain[1].content}\`\`\`\nI wonder who it is, could it be:\n ● **${arr[0]}**\n● **${arr[1]}**\n● **${arr[2]}**\n\n**30 seconds starts now!**\n (so dont fuck up)`)
+  .setFooter(`Player: ${message.member.user.tag}`)
+  message.channel.send({embeds: [embed]})
+  let userFilter = m => m.author.id === message.author.id
+  let userResponse = await message.channel.awaitMessages({filter: userFilter, max: 1, time: 30000, errors: ['time']}).catch(error => {
+    message.channel.send(`Timeout ${message.author}! The correct answer was **${randomMessageMain[1].author.username}**`)
     return
-  }
+})
+if (typeof userResponse == 'undefined') {
+  return
+}
+let userGuess = userResponse.map(msg => msg.content).join(' ')
+
   let userGuess = userResponse.map(msg => msg.content).join(' ')
 
   if (userGuess.toLowerCase() === randomMessageMain[1].author.username.toLowerCase()) {
